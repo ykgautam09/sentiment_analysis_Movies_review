3d87b13c89f8e98ae4762d79170c8bcdd42326a2
ykgautam09 <yateesh.18262@knit.ac.in>
2020-11-19 21:17:27 +0530

    ready

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+from model import predict_class
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def nlp_route():
+    return render_template('movieInput.html', size=0)
+
+
+@app.route('/', methods=['POST'])
+def cosine_model():
+    reviews = request.form.get('movie')
+    review_class = predict_class(str(reviews))
+    if review_class == 1:
+        return render_template('movieInput.html', review_class="POSITIVE", size=1)
+    return render_template('movieInput.html', review_class="NEGATIVE", size=1)
+
+
+if __name__ == '__main__':
+    app.run(port='5000', debug=True)
---
+++ model.py
@@ -0,0 +1,34 @@
+import pickle
+import re
+import nltk
+from bs4 import BeautifulSoup
+from nltk.stem import PorterStemmer
+
+stemmer = PorterStemmer()
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
+with open('./vectorizer.pickle', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+with open('./model.pickle', 'rb') as f:
+    model = pickle.load(f)
+
+
+def clean_text(review):
+    review = BeautifulSoup(review, 'lxml').text
+    review = re.sub('[^a-zA-z]', ' ', review).lower()
+    words = []
+    for j in review.split():
+        if j not in stopwords:
+            words.append(stemmer.stem(j))
+    return [' '.join(words)]
+
+
+def predict_class(review):
+    review = vectorizer.transform(clean_text(review))
+    result = model.predict(review)[0]
+    return result
+
+
+if __name__ == "__main__":
+    predict_class()
